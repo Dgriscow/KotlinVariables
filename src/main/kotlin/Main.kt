f45550c9bcb1fe712b9b64759136a409b507b627
Dgriscow <dgrischow@students.mchenry.edu>
2023-08-17 17:16:43 -0500

    pushed

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    val byte:Byte = 4
+    println("$byte Byte, a 8-bit signed integer that be used to save space")
+    val short:Short = 68
+    println("$short Short, a 16-bit signed integer that can be used to save space in place of normal a normal int")
+    val int:Int = 1111
+    println("$int Int is a 32 bit ingeger number")
+    val long:Long = 423414154
+    println("$long is a long, a 64 bit integer")
+    val float:Float = 0.4324f
+    println("$float is a float, which is a single precision floating point number")
+    val double:Double = 4332.4325632
+    println("$double is a double, which is a 64 bit floating point number, a very large decimal number storage")
+    val bool:Boolean = false
+    println("$bool is a boolean value, is either true or false ")
+    val char:Char = 'a'
+    println("$char is a Char, which is a single 16bit charecter")
+}
